91f757bcf775a8816ed27443d816c0c070356f3b
BlinkLudus <conor.sheppard@hotmail.com>
2023-03-23 12:46:29 +0000

    Added animations to movement

--- 2DRPG-Unity/Assets/Scripts/PlayerMovement.cs
+++ 2DRPG-Unity/Assets/Scripts/PlayerMovement.cs
@@ -2,34 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
+    
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    public Animator animator;
     private float horizontal;
     public float speed = 10f; //The speed at which the player moves
     public float jumpingPower = 16f;
     private bool isFacingRight = true;
 
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
 
     // Update is called once per frame
     void Update()
     {
+        
+
         //Get horizontal input axis (Left & Right arrow keys, A/D, gamepad)
         horizontal = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-        }
-
+            animator.SetBool("IsJumping", true);
+        } 
+        
         if(Input.GetKeyDown(KeyCode.Space) && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            
         }
 
         Flip();
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+    }
+   
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     private bool IsGrounded()
